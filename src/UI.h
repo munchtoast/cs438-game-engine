f2a8b3d7b59f61ae4587f4e892bd6755493f41b7
Munchtoast <dextersydney2001@gmail.com>
2024-02-07 11:31:50 -0500

    feat(UI): Add class for UI utilizing viewports
    This commit adds in the initial feature for UI component. This would set
    a viewport and a renderer can possibly be established for displaying
    elements

---
+++ src/UI.h
@@ -0,0 +1,37 @@
+/*
+ * @file UI.h
+ * @brief Declaration of the UI class, a fundamental GameObject for
+ * target-rendering objects
+ *
+ * The UI class features tools required to render specific areas within the
+ * game engine
+ *
+ * @note Ensure that dependencies of UI class are satisfied
+ *
+ * @version
+ * - 1.0: Initial implementation (dexter@nekocake.cafe) (2024-02-05)
+ */
+
+#ifndef UI_H
+#define UI_H
+
+#include "GameObject.h"
+
+#include <SDL.h>
+
+namespace UI {
+class UI : public GameObject::GameObject {
+public:
+  UI(float x, float y, float width, float height);
+  virtual ~UI();
+
+  void move(int deltaX, int deltaY);
+
+  void apply(SDL_Renderer *renderer);
+
+private:
+  void cleanup();
+};
+} // namespace UI
+
+#endif
